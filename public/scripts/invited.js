cb854d8d26714e68aaa29ad7b926d7aab8282362
Lifei Li <lupuswere@gmail.com>
2015-04-05 15:40:28 -0700

    Invited Invitations presented

--- public/scripts/invited.js
+++ public/scripts/invited.js
@@ -1,5 +1,5 @@
 var app = angular.module("invitedPage", []);
-app.controller("mainCtrl", function mainCtrl ($scope, $http, $window) {
+app.controller("mainCtrl", function mainCtrl($scope, $http, $window) {
     $scope.user = {};
     $scope.invitedInvitations = [];
     $http.get("/checklogin")
@@ -10,7 +10,23 @@ app.controller("mainCtrl", function mainCtrl ($scope, $http, $window) {
                     .success(function (data) {
                         if (data) {
                             var tmpInvitedInvitations = [];
-
+                            var cont = "";
+                            while(data && data.length !== 0) {
+                                for (var i = 0; i < data.length; i++) {
+                                    if (i === 0) {
+                                        cont = data[i].content;
+                                        var tmpList = [];
+                                    }
+                                    if (data[i].content === cont) {
+                                        tmpList.push(data[i]);
+                                        data.splice(i, i + 1);
+                                        if (i !== 0) {
+                                            i--;
+                                        }
+                                    }
+                                }
+                                tmpInvitedInvitations.push(tmpList);
+                            }
                             $scope.invitedInvitations = tmpInvitedInvitations;
                         }
                     })
